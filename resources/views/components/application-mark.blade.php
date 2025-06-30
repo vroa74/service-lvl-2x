515e5705afbbf5d4307c9b988ea85471d3fec732
vroa74 <33233830+vroa74@users.noreply.github.com>
2025-06-30 10:43:56 -0600

    UP 12

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -6,6 +6,6 @@ for ($i = 1; $i <= 24; $i++) {
 $randomFavicon = $favicons[array_rand($favicons)];
 @endphp
 <picture>
-    <source srcset="{{ asset($randomFavicon) }}" type="image/png">
-    <img src="{{ asset($randomFavicon) }}" alt="Logo" {{ $attributes }}>
+    <source srcset="{{ asset($randomFavicon) }}" type="image/png" width="50px" height="50px">
+    <img src="{{ asset($randomFavicon) }}" alt="Logo" {{ $attributes }}  width="50px" height="50px" >
 </picture>
